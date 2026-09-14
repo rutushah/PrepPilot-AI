5672547d5edfdbc5efd3548d2ea1e8358bef44bc
rutus-code <rutushah105@gmail.com>
2026-09-13 21:36:49 -0500

    Added frontend for quiz

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -2,22 +2,25 @@
 import { useState } from "react";
 import { Button } from "@/components/ui/button"
 import { set } from "react-hook-form";
+import ProgressBar from "@/components/progressBar";
+import { ChevronLeft, X } from "lucide-react";
+import ResultCard from "./ResultCard";
 
 const questions = [
     {
         questionText: "What is React?",
         answers:[
             {
-                answerText: "A Library for building user interfaces", incorrect: true, id:1 
+                answerText: "A Library for building user interfaces", isCorrect: true, id:1 
             },
             {
-                answerText: "A Front-end framework", incorrect: false, id:2 
+                answerText: "A Front-end framework", isCorrect: false, id:2 
             },
             {
-                answerText: "A Backend Framework", incorrect: false, id:3 
+                answerText: "A Backend Framework", isCorrect: false, id:3 
             },
             {
-                answerText: "A Database", incorrect: false, id:4 
+                answerText: "A Database", isCorrect: false, id:4 
             },
         ]
     },
@@ -25,16 +28,16 @@ const questions = [
         questionText: "How do you embed dynamic JavaScript expressions inside JSX?",
         answers:[
             {
-                answerText: 'Using quotes " "', incorrect: false, id:1 
+                answerText: 'Using quotes " "', isCorrect: false, id:1 
             },
             {
-                answerText: "Using parenthetical brackets ()", incorrect: false, id:2 
+                answerText: "Using parenthetical brackets ()", isCorrect: false, id:2 
             },
             {
-                answerText: "Using curly braces {}", incorrect: true, id:3 
+                answerText: "Using curly braces {}", isCorrect: true, id:3 
             },
             {
-                answerText: "Using angle brackets < > ", incorrect: false, id:4 
+                answerText: "Using angle brackets < > ", isCorrect: false, id:4 
             },
         ]
     },
@@ -42,16 +45,16 @@ const questions = [
         questionText: "What does JSX stand for?",
         answers:[
             {
-                answerText: "JavaScript Extension", incorrect: false, id:1 
+                answerText: "JavaScript Extension", isCorrect: false, id:1 
             },
             {
-                answerText: "JavaScript XML", incorrect: true, id:2 
+                answerText: "JavaScript XML", isCorrect: true, id:2 
             },
             {
-                answerText: "Java eXtended", incorrect: false, id:3 
+                answerText: "Java eXtended", isCorrect: false, id:3 
             },
             {
-                answerText: "JavaScript XSL", incorrect: false, id:4 
+                answerText: "JavaScript XSL", isCorrect: false, id:4 
             },
         ]
     },
@@ -61,8 +64,11 @@ const questions = [
 export default function Home() {
 
     // check if the user has started filling out the survey or not
-    const [started, setStarted] = useState(false);
-    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const [started, setStarted] = useState<boolean>(false);
+    const [currentQuestion, setCurrentQuestion] = useState<number>(0);
+    const [score, setScore] = useState<number>(0);
+    const [selectedAnswer, setSelectedAnswer] = useState < number | null > (null);
+    const[isCorrect,  setIsCorrect] = useState<boolean | null>(null);
 
     const handleNext =() =>{
         if(!started){
@@ -72,12 +78,30 @@ export default function Home() {
         if (currentQuestion < questions.length-1){
             setCurrentQuestion(currentQuestion + 1);
         }
-        
+        setSelectedAnswer(null);
+        setIsCorrect(null);
     }
 
+    const handleAnswer = (answer) => {
+        setSelectedAnswer(answer.id);
+        const isCurrentCorrect = answer.isCorrect;
+        if  (isCurrentCorrect){
+            setScore(score + 1);
+        }
+        setIsCorrect(isCurrentCorrect);
+    }
 
   return (
     <div className="flex flex-col flex-1">
+        <div className="position-sticky top-0 z-10 shadow-md py-4 w-full">
+            <header className="grid grid-cols-[auto,1fr,auto] 
+            grid-flow-col items-center justify-between py-2
+            gap-2">
+                <Button size="icon" variant="outline"> <ChevronLeft/> </Button>
+                <ProgressBar value={(currentQuestion / questions.length) * 100} ></ProgressBar>
+                <Button size="icon" variant="outline"> <X/> </Button>
+            </header>
+        </div>
         <main className="flex justify-center flext-1">
           {!started ? <h1 className="text-3xl font-bold">Welcome to the quiz Page 👋</h1> : (
             <div>
@@ -86,8 +110,19 @@ export default function Home() {
                    {
                     questions[currentQuestion].answers.map(
                         answer => {
+                            const variant = 
+                            selectedAnswer === answer.id 
+                            ? answer.isCorrect
+                                ? "neoSuccess"
+                                : "neoDanger"
+                            : "neoOutline";
                             return(
-                                <Button key={answer.id} variant = {"secondary"}> {answer.answerText}</Button>
+                                <Button key={answer.id} variant={variant} size ="xl"
+                                onClick={() => handleAnswer(answer)}> 
+                                    <p className="whitespace-normal">
+                                        {answer.answerText}
+                                    </p>
+                                </Button>
                             )
                         }
                     )
@@ -97,8 +132,10 @@ export default function Home() {
             </div>
           )}
         </main>
-        <footer className="footer pb-9 px-6 relative mb-0">
-          <Button onClick={handleNext}> {!started ? 'Start': 'Next'} </Button>
+        <footer className="footer pb-10 px-8 relative mb-1">
+          <ResultCard isCorrect ={isCorrect} 
+          correctAnswer={questions[currentQuestion].answers.find(answer => answer.isCorrect === true)?.answerText}/>
+          <Button variant ="neo" size="lg" onClick={handleNext}> {!started ? 'Start': 'Next'} </Button>
         </footer>
     </div>
   )
