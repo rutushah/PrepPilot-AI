593f0d5a6b685dde859cdbc12b2735f4af9defad
rutus-code <rutushah105@gmail.com>
2026-09-21 07:39:22 -0500

    added link navigations for the header

--- src/app/quiz/layout.tsx
+++ src/app/quiz/layout.tsx
@@ -1,8 +1,13 @@
+import Header from "../../components/ui/header"
+
 export default function Layout({children}:
     {children: React.ReactNode}) {
         return (
+            <div className="flex min-h-screen flex-col">
+                <Header/>
             <div className="flex flex-col flex-1 max-w-96 w-full m-auto h-screen gap-6">
                 {children}
+            </div>
             </div>
         )
     }
